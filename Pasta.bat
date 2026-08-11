8d9be0813c81a7df31350803d73bc53b4de696cb
anyuser235-maker <anyuser235@gmail.com>
2026-08-11 19:26:43 +0000

    Update Pasta.bat

--- Pasta.bat
+++ Pasta.bat
@@ -23,6 +23,6 @@ curl -L -H "User-Agent: Mozilla/5.0" -o "OpS.7z" "https://github.com/optiscaler/
 del /f /q "OpS.7z"
 
 :: 8. Run setup_windows.bat with inputs: 1 (dxgi.dll), 2 (Nvidia), and Enter (to pass pause)
-:: (echo 1 & echo 2 & echo.) | cmd /c setup_windows.bat
+(echo 1 & echo 2 & echo.) | cmd /c setup_windows.bat
 
 echo Process finished!
